14e4ba980831f3d81fc4b231d9d799685be76363
saeid <saeid.noormohammad@gmail.com>
2022-02-27 16:48:59 +0300

    d

--- pages/carpets/index.js
+++ pages/carpets/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { initializeApollo } from "../../apolloConfig/apollo";
 import { GET_PRODUCTS } from "../../graphql_f/product/Query/getProduct";
 import CarpetsPage from "../../components/CarpetsPage/Carpets";
-import { useQuery } from "@apollo/client";
+// import { useQuery } from "@apollo/client";
 import Nav from "../../Layouts/Nav/Nav";
 import Footer from "../../Layouts/Footer/Footer";
 
@@ -10,7 +10,8 @@ export default function Carpets({ initialApolloState }) {
   // const { data, loading, error } = useQuery(GET_PRODUCTS);
   const products = initialApolloState.ROOT_QUERY.products;
   return (
-    <>{!loading ? <CarpetsPage products={products} /> : <p>loading...</p>}</>
+    <CarpetsPage products={products} />
+    // <>{!loading ? <CarpetsPage products={products} /> : <p>loading...</p>}</>
   );
 }
 
